5b8018685a4db8f85f2a110f437ca4146b7a824c
Cholst <cholstthedev@gmail.com>
2024-09-14 15:02:53 +0200

    Finpudsning

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tic-Tac-Toe
-Comtek første semester opgave
+Comtek første semester opgave.
+
+Programmet er blevet forholdsvist langt, og kan nok simplificeres på mange måder. Koden er delt op i regioner, så de nemt kan pakkes sammen for bedre læselighed (også så de arme medstuderende der skal vurdere det ikke skal læse 500 linjer kode)
+
+Det gør brug af klasser, som ikke er noget vi decideret har lært om endnu, men jeg håber alligevel det er let forståeligt.
+
+Lidt om programmets features:
+ - Hvis du ændrer i koden, så er der fuld funktionalitet til at øge størrelsen af brættet. Computer-modspilleren er dog ikke særlig god til at spille på større bræt - men den kan godt. Du skal ændre variablen "enableBoardSizeChoice" til "True".
+ - Det er muligt at spille mod en computerstyret modstander.
+  
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,21 @@
 import random
 import time
 
+"""
+Comtek første semester opgave.
+
+Programmet er blevet forholdsvist langt, og kan nok simplificeres på mange måder. Koden er delt op i regioner, så de nemt kan pakkes sammen for bedre læselighed (også så de arme medstuderende der skal vurdere det ikke skal læse 500 linjer kode)
+
+Det gør brug af klasser, som ikke er noget vi decideret har lært om endnu, men jeg håber alligevel det er let forståeligt.
+
+Lidt om programmets features:
+ - Det er muligt at spille mod en computerstyret modstander
+
+ - Hvis du ændrer i koden, så er der fuld funktionalitet til at øge størrelsen af brættet. 
+   Computer-modspilleren er dog ikke særlig god til at spille på større bræt - men den kan godt. 
+   Du skal ændre variablen "enableBoardSizeChoice" til "True".
+"""
+
 # region classes
 class Player(object):
     """Holds data about the player"""
@@ -216,8 +231,6 @@ def ExecuteMove(targetRow, targetColumn):
     
     if CheckVictory():
         Victory()
-
-        
 # endregion
     
 # region Evaluate game ending
